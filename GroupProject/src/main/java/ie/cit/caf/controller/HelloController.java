e3d8f43bbe2b4a8aedaccca37f884cdaf1db45a2
kasiabrych <kasiabrych.kb@gmail.com>
2015-04-26 12:28:04 +0100

    HelloCotrolller working now

--- GroupProject/src/main/java/ie/cit/caf/controller/HelloController.java
+++ GroupProject/src/main/java/ie/cit/caf/controller/HelloController.java
@@ -3,15 +3,17 @@ package ie.cit.caf.controller;
 import java.util.Date;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class HelloController {
 	
-	 @RequestMapping(value={"/","/home"})
+	 @RequestMapping(value={"/","/home"}, method = RequestMethod.GET)
 		public String showHomePage(ModelMap model) { 
 			Date date = new java.util.Date();		
 			model.addAttribute("message", "This is TheHits home page.");
