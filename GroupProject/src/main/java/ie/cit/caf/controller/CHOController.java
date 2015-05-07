90acaa079010bb831a7ba8a41d01c445681237d1
kasiabrych <kasiabrych.kb@gmail.com>
2015-05-07 10:12:07 +0100

    A little more jpa added: findByMediumContains/choByMediumLike

--- GroupProject/src/main/java/ie/cit/caf/controller/CHOController.java
+++ GroupProject/src/main/java/ie/cit/caf/controller/CHOController.java
@@ -3,11 +3,13 @@ package ie.cit.caf.controller;
 import java.util.List;
 
 import ie.cit.caf.domain.CHObject;
+import ie.cit.caf.jparepo.ChoJpaRepo;
 import ie.cit.caf.repository.CHORepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +20,9 @@ public class CHOController {
 	@Autowired
 	CHORepository choRep;
 	
+	@Autowired
+	ChoJpaRepo choJpaRepo;
+	
 	@RequestMapping(value="/listall", method = RequestMethod.GET) 
 	public String listAll(ModelMap model) {			
 			
@@ -25,5 +30,15 @@ public class CHOController {
 			model.addAttribute("CHOs", listCHO);
 		    return "displayCHO";			
 		}    
+	@RequestMapping (value="/medium/like/{mediumName}", method = RequestMethod.GET)
+	public String choByMediumLike(@PathVariable String mediumName, ModelMap model){
+		
+		List<ie.cit.caf.entity.CHObject> choMediumList = choJpaRepo.findByMediumContains(mediumName);
+		
+		model.addAttribute("CHOs", choMediumList);
+		
+		return "displayCHO";
+		
+	}
 		
 }                      
--- GroupProject/src/main/java/ie/cit/caf/jparepo/ChoJpaRepo.java
+++ GroupProject/src/main/java/ie/cit/caf/jparepo/ChoJpaRepo.java
@@ -1,5 +1,7 @@
 package ie.cit.caf.jparepo;
 
+import java.util.List;
+
 import ie.cit.caf.entity.CHObject;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChoJpaRepo extends CrudRepository <CHObject, Integer>{
 	
-	
+	public List<CHObject> findByMediumContains(String mediumName);
 
 }
